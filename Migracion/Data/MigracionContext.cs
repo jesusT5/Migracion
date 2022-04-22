55f70430fb53c10288357a47fa49f0f62736a75d
jtoledo5630@outlook.com <jtoledo5630@outlook.com>
2022-04-22 10:58:07 -0700

    Versión 1.0
    Subir todos los archivos de la migración por primera vez

---
+++ Migracion/Data/MigracionContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Migracion.Model;
+
+namespace Migracion.Data
+{
+    public class MigracionContext : DbContext
+    {
+        public MigracionContext (DbContextOptions<MigracionContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Migracion.Model.Humano> Humano { get; set; }
+    }
+}
---
+++ Migracion/Model/Humano.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Migracion.Model
+{
+    public class Humano
+    {
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Sexo { get; set; }
+        public int Edad { get; set; }
+        public decimal Altura { get; set; }
+        public decimal Peso { get; set; }
+    }
+}
